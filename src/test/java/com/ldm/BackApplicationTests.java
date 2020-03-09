3d7192957711bde59484ecddd9b41e8bd6c0763f
自明小友 <36662094+1705030118@users.noreply.github.com>
2020-03-09 21:48:59 +0800

    big update

--- src/test/java/com/ldm/BackApplicationTests.java
+++ src/test/java/com/ldm/BackApplicationTests.java
@@ -1,35 +1,12 @@
 package com.ldm;
 
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.test.context.junit4.SpringRunner;
-import java.util.Iterator;
-import java.util.Set;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BackApplicationTests.class)
 public class BackApplicationTests {
-    @Autowired
-    private RedisTemplate<String,Object> redisTemplate;
-    @Test
-    public void contextLoads() {
-        long now=System.currentTimeMillis();
-        System.out.println(now);
-        redisTemplate.opsForZSet().add("activity:search:123456","mybatis",0.0);
-        redisTemplate.opsForZSet().add("activity:search:123456","spring",Double.valueOf(1569293782783.0));
-        redisTemplate.opsForZSet().removeRange("activity:search:123456",0,0);
-        redisTemplate.opsForZSet().incrementScore("activity:search:123456","mybatis",1.0);
-        Set<ZSetOperations.TypedTuple<Object>> typedTupleSet=redisTemplate.opsForZSet().reverseRangeWithScores("activity:search:123456",0,2);
-        Iterator iterator=typedTupleSet.iterator();
-        while (iterator.hasNext()){
-            ZSetOperations.TypedTuple<Object> typedTuple = (ZSetOperations.TypedTuple<Object>) iterator.next();
-            System.out.println(typedTuple.getValue());
-        }
-    }
 
 }
