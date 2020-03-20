e6718d0f1dc673fca03ab0267cb5a68f853b061f
自明小友 <36662094+1705030118@users.noreply.github.com>
2020-03-20 17:48:09 +0800

    make small update

--- src/main/java/com/ldm/service/cache/impl/CacheServiceImpl.java
+++ src/main/java/com/ldm/service/cache/impl/CacheServiceImpl.java
@@ -5,12 +5,15 @@ import com.ldm.util.JsonUtil;
 import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.JedisCluster;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 @RestController
 public class CacheServiceImpl implements CacheService {
-
+    private static final String SET_IF_NOT_EXIST = "NX";
+    private static final String SET_WITH_EXPIRE_TIME = "PX";
     private JedisCluster jedisCluster;
 
     @Override
@@ -33,13 +36,23 @@ public class CacheServiceImpl implements CacheService {
         return true;
     }
 
+    /**
+     * 分布式锁
+     * @param key    键
+     * @param value  值
+     * @param nxxx NX|XX，NX=Only set the key if it does not already exist；XX=Only set the key if it already exist
+     * @param expx EX|PX，expire time units: EX = seconds; PX = milliseconds
+     * @param expireSeconds
+     * @param <T>
+     * @return
+     */
     @Override
     public <T> boolean set(String key, T value, String nxxx, String expx, int expireSeconds) {
         // 将对象转换为json字符串
         String strValue = JsonUtil.beanToString(value);
         if (strValue == null || strValue.length() <= 0)
             return false;
-        //jedisCluster.set(key,strValue,nxxx,expx,expireSeconds);
+        jedisCluster.set(key,strValue,nxxx,expx,expireSeconds);
         return true;
     }
 
@@ -154,5 +167,15 @@ public class CacheServiceImpl implements CacheService {
     public boolean deleteFollowsFansById(int fromId, int toId) {
         return false;
     }
-
+    public String testLock(){
+        String result=jedisCluster.set("lock-token","lidongming","NX","EX",60*2);
+        return result;// "OK"为成功
+    }
+    public Object testUnLock(){
+        //lua script
+        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+        String request= UUID.randomUUID().toString();
+        Object result=jedisCluster.eval(script, Collections.singletonList("lock-token"),Collections.singletonList("lidongming"));
+        return result;// 1为成功
+    }
 }
